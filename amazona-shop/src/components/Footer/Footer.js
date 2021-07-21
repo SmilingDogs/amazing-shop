84a18afd5e591d51a80bc4230ed178ff8bb9d8b6
Max Kudryashov <kudryashovmps2.2@gmail.com>
2021-07-21 15:08:30 +0300

    testing ability to push  to SmilingDogs

--- amazona-shop/src/components/Footer/Footer.js
+++ amazona-shop/src/components/Footer/Footer.js
@@ -2,6 +2,6 @@ import React, { memo } from "react";
 
 function Footer() {
   console.log("Rendering Footer!");
-  return <footer className="row center">All right reserved</footer>;
+  return <footer className="row center">All right reserved. Copyright</footer>;
 }
 export default memo(Footer);
